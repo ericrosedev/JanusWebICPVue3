46f7b6ace922eda54da80a8c0e6190f55c2e569a
Eric Rose <tacodaoicp@gmail.com>
2024-05-10 10:00:59 -0700

    init
    simple (barely) working example of running JanusWeb on a canister

---
+++ src/januswebicpvue3/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  
+  <main>
+
+    <janus-viewer>
+      <FireBoxRoom>
+        <Room use_local_asset="room1">
+          <Object id="cube" pos="0 1 5" />
+          <Text col="1 0 0" pos="0 2 4">My First Room</Text>
+        </Room>
+      </FireBoxRoom>
+    </janus-viewer>
+
+  </main>
+
+</template>
+
+<style>
+
+</style>
+
+<script setup>
+
+import { ref } from 'vue';
+
+let greeting = ref('');
+
+</script>
